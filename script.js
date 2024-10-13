bafbb63d0af0e537486840f4376bdcd29e9eff1d
hexofits <hexofits@gmail.com>
2024-10-13 11:59:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+async function getProducts(){
+    let response = await fetch("items.json")
+
+    let products = await response.json()
+    return products
+}
+
+function getCardHTML(product){
+    let productData = JSON.stringify(product)
+    return `
+    <div class="one_item">
+            <img src="img/${product.image}" alt="">
+            <p class="name">${product.title}</p>
+            <p class="price">${product.price}</p>
+            <a href="" class="buy">Buy</a>
+        </div>
+        `
+}
+
+getProducts().then(function(products){
+    let productsList = document.querySelector(`.items`)
+    if (productsList){
+        products.forEach(product => {
+            productsList.innerHTML += getCardHTML(product)
+        });
+    }
+    let buyButtons = document.querySelectorAll(`.buy`)
+    if ( buyButtons){
+        buyButtons.forEach(button =>{
+            button.addEventListener(`click`, buyItem)
+        }) 
+    }
+})
+
+
+  
+function searchProducts(event){
+    event.preventDefaul()
+
+    let field = document.querySelector(`.search_field`)
+    let query = field.value.roLowerCase()
+    let productsList = document.querySelector(`.items`)
+
+    getProducts().then(function(products){
+        let productsList = document.querySelector('.items')
+        products.forEach(product =>{
+            if (product.title.toLowerCase().includes(query)){
+                productsList.innerHTML += getCardHTML(product)
+            }
+        })
+        let buyButtons = document.querySelectorAll('.buy')
+        if ( buyButtons){
+            buyButtons.forEach(button =>{
+                button.addEventListener('click', buyItem)
+            })
+        }
+    })
+}
+
+let searchForm = document.querySelector('.search')
+searchForm.addEventListener('submit', searchProducts)
+let searchBtn = document.querySelector('.search_btn')
+searchBtn.addEventListener('click', searchProducts)
+
+
